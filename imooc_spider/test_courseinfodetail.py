1b90143c734ef2e84e7ef069c61683030145a5cb
MelonYu <poisson@hust.edu.cn>
2018-07-18 23:12:01 +0800

    start courseinfo detail: chapter, score

---
+++ imooc_spider/test_courseinfodetail.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+"""
+@Author  :   Amelia 
+@Contact :   yu_mengling@hust.edu.cn
+@File    :   test_courseinfodetail.py
+ 
+@Time    :   18-7-18 下午11:08
+"""
+
+if __name__ == '__main__':
+    pass
