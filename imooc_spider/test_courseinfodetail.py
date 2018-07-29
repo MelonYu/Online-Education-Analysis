1ea163da064e36ac9d8355407f69103c2f38d53e
MelonYu <poisson@hust.edu.cn>
2018-07-29 21:22:55 +0800

    completed basicly

--- imooc_spider/test_courseinfodetail.py
+++ imooc_spider/test_courseinfodetail.py
@@ -26,23 +26,48 @@ base_url = 'https://www.imooc.com/learn/'
 
 headers = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
 
-for course_id in range(80, 86):
+for course_id in range(85, 86):
     user_url = base_url + str(course_id)
     res = requests.get(user_url, headers=headers)
     if res.status_code != 200:
         print('course %s not exist' % course_id)
-        break
+        continue
 
     html_doc = res.text
-    # print(html_doc)
-    soup = BeautifulSoup(html_doc, 'html.parser', from_encoding='utf-8')
+    soup = BeautifulSoup(html_doc, 'html.parser')
     # course_list = soup.find_all("div",class_="course-list-cont")
     # #    print course_list
     # for course in course_list:
     #     print course
+    course_detail = {}
+    path_detail = soup.find('div', class_='path').get_text().strip()
+    course_tip = soup.find_all("dd")
+    course_tip_first = course_tip[0].get_text()
+    course_tip_second = course_tip[1].get_text().strip()
+    teacher_info = soup.find('span', class_="tit").find("a")
+    teacher_name = teacher_info.get_text().strip()
+    teacher_url = teacher_info["href"]
+    teacher_job = soup.find("span", class_="job").get_text()
+    course_info = soup.find_all("span", class_="meta-value")
+    course_duration = course_info[1].get_text()
+    course_score = course_info[3].get_text()
+    menu_pp = soup.find("ul", class_="course-menu").find_all("span")
+    comment_pp = menu_pp[0].get_text()
+    score_pp = menu_pp[1].get_text()
 
-    path_detail = soup.find('div', class_='path')
-    print(path_detail.get_text())
+    # course_detail['']
+
+
+    print(path_detail)
+    print(course_tip_first)
+    print(course_tip_second)
+    print(teacher_name)
+    print(teacher_url)
+    print(teacher_job)
+    # print(course_info)
+    print(comment_pp)
+    print(score_pp)
+    print("done")
 
 
 # if __name__ == '__main__':
